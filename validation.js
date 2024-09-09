4633c863c225748f43b6d38f33a4d0e55308bb08
jui9797 <jannatjui@414gmail.com>
2024-09-09 20:22:28 +0600

    adding validation

--- validation.js
+++ validation.js
@@ -1,6 +1,19 @@
 function multiply(num1,num2){
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+    return 'please provide a number'
+    }
     const mult =num1*num2;
     return mult;
 }
-const result =multiply(5,7);
+const result =multiply(9,8);
 console.log(result);
+
+function join(first, scound){
+    if(typeof first !== 'string' || typeof scound !== 'string'){
+        return 'provide a string';
+    }
+    const fullName =first+' '+scound;
+    return fullName;
+}
+const naming =join(7, 'munni');
+console.log(naming);
